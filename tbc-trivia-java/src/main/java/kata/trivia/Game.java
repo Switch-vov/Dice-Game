2d1c7591539f3a9c30a59c3e6f2c340259cd0d96
Switch <q547550831@outlook.com>
2016-06-27 11:43:25 +0800

    Removed field purses from class Game.

--- tbc-trivia-java/src/main/java/kata/trivia/Game.java
+++ tbc-trivia-java/src/main/java/kata/trivia/Game.java
@@ -10,8 +10,7 @@ public class Game {
     private final QuestionMaker questionMaker = new QuestionMaker();
     private ArrayList<Player> players = new ArrayList<Player>();
 
-    // TODO: Move purses into class Player
-    private int[] purses = new int[6];
+    // TODO-working-on: Move purses into class Player
 
     // TODO: Move inPenaltyBox into class Player
     private boolean[] inPenaltyBox = new boolean[6];
