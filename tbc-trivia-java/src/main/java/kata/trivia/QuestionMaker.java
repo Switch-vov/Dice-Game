1c841640f7ddc931617a83ebb259c7b79e83eae4
Switch <q547550831@outlook.com>
2016-06-27 09:47:09 +0800

    Moved the 4 question lists from class Game to QuestionMaker.

---
+++ tbc-trivia-java/src/main/java/kata/trivia/QuestionMaker.java
@@ -0,0 +1,14 @@
+package kata.trivia;
+
+import java.util.LinkedList;
+
+/**
+ * Created by Switch on 2016-06-27.
+ */
+public class QuestionMaker {
+    // TODO-working-on: Move question lists to a new class QuestionMaker
+    private LinkedList popQuestions = new LinkedList();
+    private LinkedList scienceQuestions = new LinkedList();
+    private LinkedList sportsQuestions = new LinkedList();
+    private LinkedList rockQuestions = new LinkedList();
+}
