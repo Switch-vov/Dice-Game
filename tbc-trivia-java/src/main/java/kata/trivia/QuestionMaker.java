957948f3c2d5fe24450327767bdfc1e054504e9c
Switch <q547550831@outlook.com>
2016-06-27 16:53:28 +0800

    Moved question preparation code from class Game to QuestionMaker.

--- tbc-trivia-java/src/main/java/kata/trivia/QuestionMaker.java
+++ tbc-trivia-java/src/main/java/kata/trivia/QuestionMaker.java
@@ -11,6 +11,17 @@ public class QuestionMaker {
     private LinkedList<String> sportsQuestions = new LinkedList<String>();
     private LinkedList<String> rockQuestions = new LinkedList<String>();
 
+    public static final int MAX_NUMBER_OF_QUESTIONS = 50;
+
+    public QuestionMaker() {
+        for (int i = 0; i < MAX_NUMBER_OF_QUESTIONS; i++) {
+            addPopQuestions("Pop Question " + i);
+            addScienceQuestions(("Science Question " + i));
+            addSportsQuestions(("Sports Question " + i));
+            addRockQuestions("Rock Question " + i);
+        }
+    }
+
     public void addPopQuestions(String popQuestion) {
         popQuestions.add(popQuestion);
     }
