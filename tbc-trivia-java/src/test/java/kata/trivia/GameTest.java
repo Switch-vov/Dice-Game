e20b9247fba94889df8edc8701d6157943ceff29
Switch <q547550831@outlook.com>
2016-06-27 16:37:47 +0800

    Moved initialization code to @Before in test class GameTest.

--- tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
+++ tbc-trivia-java/src/test/java/kata/trivia/GameTest.java
@@ -1,12 +1,25 @@
 package kata.trivia;
 
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class GameTest {
+    private Game game = null;
+    private boolean isGameStillInProgress = true;
+
+    @Before
+    public void initialize() {
+        // Arrange
+        game = new Game();
+        game.add("Chet");
+        isGameStillInProgress = true;
+    }
+
+
     /**
      * Rigourous Test :-)
      */
@@ -17,11 +30,6 @@ public class GameTest {
 
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_and_answers_each_question_correctly_for_6_times() {
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         for(int i = 0; i < 6; i++) {
             game.roll(1);
@@ -34,11 +42,6 @@ public class GameTest {
 
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_for_7_times_ans_answers_the_question_wrongly_for_1_time_followed_by_an_odd_rolling_number_but_then_correctly_for_6_times() {
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         game.roll(1);
         game.wrongAnswer();
@@ -55,11 +58,6 @@ public class GameTest {
 
     @Test
     public void the_game_should_be_over_if_a_player_rolls_the_dice_for_8_times_and_answers_the_question_wrongly_for_1_time_followed_by_an_even_rolling_number_but_then_correctly_for_7_times_with_odd_rolling_numbers() {
-        // Arrange
-        Game game = new Game();
-        game.add("Chet");
-        boolean isGameStillInProgress = true;
-
         // Act
         game.roll(1);
         game.wrongAnswer();
